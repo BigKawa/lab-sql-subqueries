99f2409055fae935bd0400da7e43403cb17562b5
BigKawa <164774630+BigKawa@users.noreply.github.com>
2024-10-11 12:43:02 +0200

    Update subquerires.sql

--- subquerires.sql
+++ subquerires.sql
@@ -3,9 +3,9 @@
 # 1 Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
 SELECT COUNT(film_id)
 FROM inventory
-WHERE film_id IN (select film_id
+WHERE film_id IN (SELECT film_id 
 					FROM film
-					WHERE title = "Hunchback Impossible"
+					WHERE title = "Hunchback Impossible" # Filtering 
 )
 ;
 
@@ -13,8 +13,7 @@ WHERE film_id IN (select film_id
 
 SELECT title 
 FROM film
-WHERE length > (SELECT AVG(length) from
-film);
+WHERE length > (SELECT AVG(length) FROM film);
 
 
 # Use a subquery to display all actors who appear in the film "Alone Trip".
@@ -30,3 +29,19 @@ WHERE actor_id IN (
 										WHERE title = "Alone Trip"
                                         )
 					);
+                    
+
+
+# Retrieve the client_id and the total_amount_spent of those clients who spent more than the average of the total_amount spent by each client. You can use subqueries to accomplish this
+                
+SELECT customer_id, SUM(amount) as total_amount
+FROM payment 
+GROUP BY customer_id
+HAVING SUM(amount) > (SELECT AVG(total_amount)
+    FROM (
+			SELECT SUM(amount) AS total_amount
+			FROM payment
+			GROUP BY customer_id
+    ) AS each_client
+);
+
